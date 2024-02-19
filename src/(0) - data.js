41d3808a379d7cef495654ac7c5f5f44fdabf69e
abdalrhman975 <abdalrhmanobeid19.com>
2024-02-19 22:35:57 +0300

    Fix all the imgs

--- src/(0) - data.js
+++ src/(0) - data.js
@@ -8,25 +8,25 @@ export const navbar = [
 export const imgsGallery = [
     { img: 'https://elzerowebschool.github.io/HTML_And_CSS_Template_Three/imgs/gallery-01.png' },
     { img: 'https://elzerowebschool.github.io/HTML_And_CSS_Template_Three/imgs/gallery-02.png' },
-    { img: '../public/imgs/gallery-03.jpg' },
-    { img: '../public/imgs/gallery-04.png' },
-    { img: '../public/imgs/gallery-05.jpg' },
-    { img: '../public/imgs/gallery-06.png' },
+    { img: 'https://elzerowebschool.github.io/HTML_And_CSS_Template_Three/imgs/gallery-03.jpg' },
+    { img: 'https://elzerowebschool.github.io/HTML_And_CSS_Template_Three/imgs/gallery-04.png' },
+    { img: 'https://elzerowebschool.github.io/HTML_And_CSS_Template_Three/imgs/gallery-05.jpg' },
+    { img: '.https://elzerowebschool.github.io/HTML_And_CSS_Template_Three/imgs/gallery-06.png' },
 ];
 
 export const imgsFeatures = [
-    { img: '../public/imgs/features-01.jpg', title: 'Quality', text: 'We are distinguished by precision in our work and unparalleled workmanship', color: '#03a9f4' },
-    { img: '../public/imgs/features-02.jpg', title: 'Time', text: 'The time factor is one of the most important things that we excel at', color: '#009688' },
-    { img: '../public/imgs/features-03.jpg', title: 'Passion', text: 'Respectful and courteous behavior is an essential characteristic of our team', color: '#f44036;' },
+    { img: 'https://elzerowebschool.github.io/HTML_And_CSS_Template_Three/imgs/gallery-05.jpg', title: 'Quality', text: 'We are distinguished by precision in our work and unparalleled workmanship', color: '#03a9f4' },
+    { img: 'https://elzerowebschool.github.io/HTML_And_CSS_Template_Three/imgs/features-02.jpg', title: 'Time', text: 'The time factor is one of the most important things that we excel at', color: '#009688' },
+    { img: 'https://elzerowebschool.github.io/HTML_And_CSS_Template_Three/imgs/features-02.jpg', title: 'Passion', text: 'Respectful and courteous behavior is an essential characteristic of our team', color: '#f44036;' },
 ]; 
 
 export const teamMembers = [
-    { img: '../public/imgs/team-01.jpg', name: "Abdalrhman Obaid", description: 'Create professional websites with distinctive designs And CEO of the company' },
-    { img: '../public/imgs/team-02.jpg', name: "karm shababibi", description: 'Highly experienced professional designer' },
-    { img: '../public/imgs/team-03.jpg', name: "Ghaith Obaid", description: 'The right hand of the designer and the owner of extraordinary ideas' },
-    { img: '../public/imgs/team-04.jpg', name: "Abdalrhman Saif", description: 'He works in the programming department and contributes to the development of the work' },
-    { img: '../public/imgs/team-05.png', name: "Osama Faihan", description: 'The head of the customer department deals with all respect and calm' },
-    { img: '../public/imgs/team-06.png', name: "Hamza Shurbaji", description: 'The general supervisor and the monitoring eye for work progress' },
+    { img: 'https://elzerowebschool.github.io/HTML_And_CSS_Template_Three/imgs/team-01.jpg', name: "Abdalrhman Obaid", description: 'Create professional websites with distinctive designs And CEO of the company' },
+    { img: 'https://elzerowebschool.github.io/HTML_And_CSS_Template_Three/imgs/features-02.jpg', name: "karm shababibi", description: 'Highly experienced professional designer' },
+    { img: 'https://elzerowebschool.github.io/HTML_And_CSS_Template_Three/imgs/team-03.jpg', name: "Ghaith Obaid", description: 'The right hand of the designer and the owner of extraordinary ideas' },
+    { img: 'https://elzerowebschool.github.io/HTML_And_CSS_Template_Three/imgs/team-04.jpg', name: "Abdalrhman Saif", description: 'He works in the programming department and contributes to the development of the work' },
+    { img: 'https://elzerowebschool.github.io/HTML_And_CSS_Template_Three/imgs/team-05.jpg', name: "Osama Faihan", description: 'The head of the customer department deals with all respect and calm' },
+    { img: 'https://elzerowebschool.github.io/HTML_And_CSS_Template_Three/imgs/team-06.jpg', name: "Hamza Shurbaji", description: 'The general supervisor and the monitoring eye for work progress' },
 ];
 
 export const services = [
